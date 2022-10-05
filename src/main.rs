04ef8a6482bb78cbb82fedfb75d0053428d6733e
bigbass1997 <bigbass1997.website@gmail.com>
2022-10-04 21:24:09 -0500

    Added argument to allow converting an already built ELF. Added versioning info to clap. Changed output ROM extension to .z64

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use std::path::PathBuf;
-use clap::{AppSettings, Arg, Command};
+use clap::{AppSettings, Arg, Command, crate_version};
 use nust64::elf::Elf;
 use nust64::rom::Rom;
 
 fn main() {
     let matches = Command::new("nust64")
+        .version(crate_version!())
         .arg(Arg::new("project-path")
             .takes_value(true)
             .short('p')
             .long("path")
             .help("Path to project you wish to build. If omitted, current directory will be used."))
+        .arg(Arg::new("elf-path")
+            .takes_value(true)
+            .long("elf")
+            .help("Path to an already compiled ELF, to be converted into an N64 ROM. If included, anything compiled using the `project-path` argument will be ignored."))
         .arg(Arg::new("ipl3")
             .takes_value(true)
             .required(true)
@@ -37,12 +42,20 @@ fn main() {
         ipl3.resize(4032, 0x00);
     }
     
-    let elf = match Elf::build(&project_path, Some(&matches.values_of_lossy("additional-args").unwrap_or_default().iter().map(|s| s.as_str()).collect::<Vec<&str>>())) {
-        Ok(data) => data,
-        Err(err) => panic!("Error encountered during build: {:?}", err)
+    let elf = match matches.value_of("elf-path") {
+        Some(path) => match Elf::with_file(&PathBuf::from(path)) {
+            Ok(data) => data,
+            Err(err) => panic!("Error encountered while loading ELF file: {:?}", err)
+        },
+        None => match Elf::build(&project_path, Some(&matches.values_of_lossy("additional-args").unwrap_or_default().iter().map(|s| s.as_str()).collect::<Vec<&str>>())) {
+            Ok(data) => data,
+            Err(err) => panic!("Error encountered during build: {:?}", err)
+        }
     };
     
     let rom = Rom::new(&elf, ipl3.try_into().unwrap(), None);
     
-    std::fs::write(elf.path.with_extension("n64"), rom.to_vec()).unwrap();
+    let rom_path = elf.path.with_extension("z64");
+    std::fs::write(&rom_path, rom.to_vec()).unwrap();
+    println!("Generated ROM at: {}", rom_path.canonicalize().unwrap_or(rom_path).display());
 }
